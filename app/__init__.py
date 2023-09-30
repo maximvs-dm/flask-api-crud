e7352dc1f75df0aad3416afead5f4bab3a462502
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2023-09-29 22:21:04 -0300

    create first controller - not working yet

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_migrate import Migrate
+
 from .model import configure as config_db
+from .serializer import configure as config_ma
 
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../temp/teste.db'
 
     config_db(app)
+    config_ma(app)
 
     Migrate(app, app.db)
+
+    from .controllers.jogos import get_jogos
 
     return app
---
+++ app/controllers/jogos.py
@@ -0,0 +1,6 @@
+from app import app
+
+
+@app.route('/jogos')
+def get_jogos():
+    pass
