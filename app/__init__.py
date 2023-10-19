4f0cdbf7d09339e9b0f4ae040bd2b9861c4b94de
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2023-10-18 21:50:19 -0300

    add initial working version of swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_migrate import Migrate
+from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from .model import configure as config_db
 from .serializer import configure as config_ma
+
+
+# Swagger UI route
+SWAGGER_URL = '/swagger-ui'
+API_URL = '/swagger'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "My API"
+    }
+)
 
 
 def create_app():
@@ -17,5 +31,14 @@ def create_app():
 
     from .games import bp_games
     app.register_blueprint(bp_games)
+
+    @app.route('/swagger')
+    def get_swagger():
+        swag = swagger(app)
+        swag['info']['version'] = "1.0"
+        swag['info']['title'] = "Flask CRUD API"
+        return jsonify(swag)
+
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     return app
--- app/games.py
+++ app/games.py
@@ -5,6 +5,55 @@ bp_games = Blueprint('games', __name__)
 
 @bp_games.route('/exibir')
 def show_games():
+    """
+        Create a new user
+        ---
+        tags:
+          - users
+        definitions:
+          - schema:
+              id: Group
+              properties:
+                name:
+                 type: string
+                 description: the group's name
+        parameters:
+          - in: body
+            name: body
+            schema:
+              id: User
+              required:
+                - email
+                - name
+              properties:
+                email:
+                  type: string
+                  description: email for user
+                name:
+                  type: string
+                  description: name for user
+                address:
+                  description: address for user
+                  schema:
+                    id: Address
+                    properties:
+                      street:
+                        type: string
+                      state:
+                        type: string
+                      country:
+                        type: string
+                      postalcode:
+                        type: string
+                groups:
+                  type: array
+                  description: list of groups
+                  items:
+                    $ref: "#/definitions/Group"
+        responses:
+          201:
+            description: User created
+        """
     pass
 
 
